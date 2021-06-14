0e6bb2c095b216abce465b01ec1940cb110f66c8
Elias K. Salem <48525998+EKenjiSalem@users.noreply.github.com>
2021-06-14 11:34:52 -0700

    Add files via upload

---
+++ TechDegree Project#5/js/app.js
@@ -0,0 +1,23 @@
+
+
+baguetteBox.run('.gallery');
+
+let input = document.querySelector('input');
+let photos = document.getElementsByTagName('a');
+
+const filter = () => {
+    let x = input.value.toLowerCase();
+
+    for (let i = 0; i < photos.length; i++){
+      let title = photos[i].getAttribute('data-caption').toLowerCase();
+
+      if (title.includes(x)) {
+        photos[i].style.display = 'block';
+      } else {
+        photos[i].style.display = 'none';
+      }
+    }
+  };
+  
+ input.addEventListener('keyup', filter);  
+
